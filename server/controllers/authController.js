ff99698b01b5f9b956fccf8fa7532268b7ae0e64
Abelhad <hassanabelhad51@gmail.com>
2026-09-15 16:09:22 +0100

    login controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -52,4 +52,55 @@ const register = async(req, res) =>{
     }
 }
 
-module.exports = { register };
+const login = async(req, res) => {
+    try{
+        const { email, password } = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({
+                message: 'All fields are required'
+            });
+        }
+
+        const result = await pool.query(
+            `SELECT email FROM users WHERE email = $1`,
+            [email]
+        )
+
+        if(result.rows.length === 0){
+            return res.status(401).json({
+                message: 'Invalid email or password'
+            })
+        }
+
+        const user = result.rows[0];
+
+        const passwordMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if(!passwordMatch){
+            res.status(401).json({
+                message: 'Invalid email or password'
+            });
+        }
+
+        res.json({
+            message: 'Login successful',
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        })
+    } catch(error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
+}
+module.exports = { register, login };
