b8e1dbf7c4917276562c2470ee8350f1960b9ec5
Abelhad <hassanabelhad51@gmail.com>
2026-09-18 01:36:30 +0100

    add getMyBusinessProfile function

--- server/controllers/businessProfileController.js
+++ server/controllers/businessProfileController.js
@@ -61,4 +61,32 @@ const createBusinessProfile = async (req, res) =>{
     }
 }
 
-module.exports = { createBusinessProfile };
+const getMyBusinessProfile = async (req, res) => {
+    try{
+        const userId = req.user.id;
+
+        const result = await pool.query(
+            `SELECT * FROM business_profiles WHERE user_id = $1`,
+            [userId]
+        );
+
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                message: 'Business profile not found'
+            });
+        }
+
+        res.json({
+            profile: result.rows[0]
+        });
+
+    }catch(error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
+}
+
+module.exports = { createBusinessProfile, getMyBusinessProfile };
--- server/routes/businessProfileRoutes.js
+++ server/routes/businessProfileRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createBusinessProfile } = require('../controllers/businessProfileController');
+const { createBusinessProfile, getMyBusinessProfile } = require('../controllers/businessProfileController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/', authMiddleware, createBusinessProfile);
+router.get('/me', authMiddleware, getMyBusinessProfile);
 
 module.exports = router;
