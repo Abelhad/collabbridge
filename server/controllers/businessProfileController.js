93af9f751c14d803596a95099828658307918a42
Abelhad <hassanabelhad51@gmail.com>
2026-09-18 02:06:07 +0100

    add updateMyBusinessProfile

--- server/controllers/businessProfileController.js
+++ server/controllers/businessProfileController.js
@@ -89,4 +89,60 @@ const getMyBusinessProfile = async (req, res) => {
     }
 }
 
-module.exports = { createBusinessProfile, getMyBusinessProfile };
+const updateMyBusinessProfile = async (req, res) =>{
+    try{
+        const userId = req.user.id;
+
+        const {
+            business_name,
+            description,
+            location,
+            website,
+            instagram,
+            industry
+        } = req.body;
+
+        const result = await pool.query(
+            `UPDATE business_profiles
+             SET
+                business_name = $1,
+                description = $2,
+                location = $3,
+                website = $4,
+                instagram = $5,
+                industry = $6,
+                updated_at = CURRENT_TIMESTAMP
+             WHERE user_id = $7
+             RETURNING *`,
+            [
+                business_name,
+                description,
+                location,
+                website,
+                instagram,
+                industry,
+                userId
+            ]
+        );
+
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                message: 'Business profile not found'
+            });
+        }
+
+        res.json({
+            message: 'Business profile updated successfully',
+            profile: result.rows[0]
+        });
+
+    }catch(error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
+}
+
+module.exports = { createBusinessProfile, getMyBusinessProfile, updateMyBusinessProfile };
--- server/routes/businessProfileRoutes.js
+++ server/routes/businessProfileRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createBusinessProfile, getMyBusinessProfile } = require('../controllers/businessProfileController');
+const { createBusinessProfile, getMyBusinessProfile, updateMyBusinessProfile } = require('../controllers/businessProfileController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/', authMiddleware, createBusinessProfile);
 router.get('/me', authMiddleware, getMyBusinessProfile);
+router.put('/me', authMiddleware, updateMyBusinessProfile);
 
 module.exports = router;
